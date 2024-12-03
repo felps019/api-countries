3de0e6ea1650fd8f9269d15c878a70705478acac
felps019 <felipedocs313@gmail.com>
2024-12-03 19:07:56 -0300

    add card and grid components and show the cards list

---
+++ project-countries/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-6 mb-8'>
+      <p className='text-center'>Direitos autorais reservados.</p>
+    </footer>
+  )
+}
+
+export default Footer
