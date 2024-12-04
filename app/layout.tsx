526d3edfeb8d99ca4d3faa47a9efee28fd59803e
felps019 <felipedocs313@gmail.com>
2024-12-04 14:57:40 -0300

    add subset to custom font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,25 @@
 import type { Metadata } from "next";
-import { Noto_Sans} from "next/font/google"
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 
-
 const notoSans = Noto_Sans({
-  weight: ["400", "700"],
-})
+	weight: ["400", "700"],
+	subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: "Project-Countries",
-  description: "Flags of the world"
-}
+	title: "Project-Countries",
+	description: "Flags of the world",
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${notoSans.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${notoSans.className} antialiased`}>{children}</body>
+		</html>
+	);
 }
