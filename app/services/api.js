58da0771f0b6ce62cc2a03e532d8d79ff6e997be
felipe.hsilva019 <felipe.hsilva@kabum.com.br>
2025-01-14 08:27:11 -0300

    feat: add new request to fetch the country data

--- app/services/api.js
+++ app/services/api.js
@@ -17,10 +17,14 @@ const ApiClient = (baseUrl) => ({
 });
 
 const api = ApiClient("https://restcountries.com/v3.1");
-//O countriesApi faz a conexao com a instancia do apiclient
+//O countriesApi faz a conexao com a instancia do apiclient =
+const baseFields = "cca3,flags,name,capital,region,population";
 const countriesApi = {
-	getAll: () =>
-		api.get("/all?fields=cca3,flags,name,capital,region,population"), //Metodo para pegar todas essas informacoes especificas
+	getAll: () => api.get(`/all?fields=${baseFields}`), //Metodo para pegar todas essas informacoes especificas
+	getCountry: (id) =>
+		api.get(
+			`/alpha/${id}?fields=${baseFields},languages,currencies, tld, borders`,
+		),
 };
 
 export { countriesApi };
